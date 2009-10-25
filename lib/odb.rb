51a92e766a5b8880b2951b78379be12388b1051f
Scott Taylor <scott@railsnewbie.com>
2009-10-25 18:21:08 -0400

    Add Odb.init

--- lib/odb.rb
+++ lib/odb.rb
@@ -5,6 +5,20 @@ module Odb
   
   class << self
     attr_accessor :root
+    
+    def join(*args)
+      File.join(*args)
+    end
+    
+    def init(dir = Dir.getwd)
+      if File.exists?(join(dir, "odb"))
+        raise LoadError, "ODB directory already exists!"
+      else
+        Dir.mkdir(join(dir, "odb"))
+        FileUtils.touch(join(dir, "odb", "objects"))
+        FileUtils.touch(join(dir, "odb", "objects.idx"))
+      end
+    end
   end
   
   using :ObjectFormat, :file => "object_format.pb"
---
+++ spec/odb_spec.rb
@@ -0,0 +1,53 @@
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require 'fakefs/safe'
+
+describe Odb do
+  before do
+    FakeFS.activate!
+  end
+  
+  after do
+    FakeFS::FileSystem.clear
+    FakeFS.deactivate!
+  end
+  
+  describe "init" do
+    it "should create the odb directory" do
+      Odb.init
+      
+      File.directory?("#{Dir.getwd}/odb").should be_true
+    end
+    
+    it "should raise if the odb directory already exists" do
+      Odb.init
+      
+      lambda {
+        Odb.init
+      }.should raise_error(LoadError, "ODB directory already exists!")
+    end
+    
+    it "should put it at the path specified" do
+      Dir.mkdir "/foo"
+      Odb.init "/foo"
+      
+      File.directory?("/foo/odb").should be_true
+    end
+    
+    it "should create the objects file" do
+      Odb.init "/"
+      
+      File.file?("/odb/objects").should be_true
+    end
+    
+    it "should create the objects.idx file" do
+      Odb.init "/"
+      File.file?("/odb/objects.idx").should be_true
+    end
+    
+    it "should have nil as a pre-saved object"
+    
+    it "should have false as a pre-saved object"
+    
+    it "should have true as a pre-saved object"
+  end
+end
