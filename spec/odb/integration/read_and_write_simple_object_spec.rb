4b2be735193eec270a8f77f3a5b5eba3ec1c073c
Scott Taylor <scott@railsnewbie.com>
2009-10-28 02:37:03 -0400

    Add integration spec

---
+++ spec/odb/integration/read_and_write_simple_object_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
+require 'fakefs/safe'
+
+class Foo
+  attr_accessor :bar
+end
+
+describe "read and write simple objects" do
+  before do
+    FakeFS.activate!
+    Odb.init "/"
+    Odb.path = "/"
+  end
+    
+  after do
+    FakeFS::FileSystem.clear
+    FakeFS.deactivate!
+  end
+
+  it "should be able to read a previously written object" do
+    pending do
+      @obj = Foo.new
+      @obj.bar = 17
+      
+      oid = Odb::Object.new.write(@obj)
+      
+      object = Odb::Object.new.load_from_id oid
+      object.should be_a_kind_of(Foo)
+      object.bar.should == 17
+    end
+  end
+end
