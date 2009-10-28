f10ef2a71e427e941f8b9817d0ece74c7e01cd90
Scott Taylor <scott@railsnewbie.com>
2009-10-28 00:55:44 -0400

    Refactor private methods to an inner object

--- lib/odb/marshal.rb
+++ lib/odb/marshal.rb
@@ -47,26 +47,10 @@ module Odb
         data
       end
       
-      def load_user_defined_class klass_name, data
-        returning eval(klass_name).allocate do |obj|
-          load_ivars(obj, data)
-        end
-      end
-    
-      def load_ivars obj, data
-        ivars(data).each do |key, value|
-          obj.instance_variable_set(key, value)
-        end
+      def load_user_defined_class(klass_name, data)
+        UserDefinedLoader.load(klass_name, data)
       end
       
-      def ivars data
-        returning Hash.new do |hash|
-          data.each do |key, oid|
-            hash[key] = Object.new("").load_from_id(oid)
-          end
-        end
-      end
-    
       def ivars_for obj
         returning Hash.new do |hash|
           obj.instance_variables.each do |variable|
@@ -81,6 +65,47 @@ module Odb
       
       def decode str
         BERT.decode(Base64.decode64(str))
+      end
+      
+      class UserDefinedLoader
+        def self.load klass_name, data
+          new(klass_name, data).load
+        end
+        
+        def initialize klass_name, data
+          @klass_name = klass_name
+          @data       = data
+        end
+        
+        def load
+          returning new_instance do |obj|
+            load_ivars(obj)
+          end
+        end
+      
+      private
+      
+        def new_instance
+          klass.allocate
+        end
+      
+        def klass
+          eval(@klass_name)
+        end
+        
+        def load_ivars obj
+          ivars.each do |key, value|
+            obj.instance_variable_set(key, value)
+          end
+        end
+        
+        def ivars
+          returning Hash.new do |hash|
+            @data.each do |key, oid|
+              hash[key] = Object.new("").load_from_id(oid)
+            end
+          end
+        end
       end
     end
   end
