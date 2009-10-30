2c8d52de4ad8f6a0954e4481acd7016b102a6969
Scott Taylor <scott@railsnewbie.com>
2009-10-29 22:41:08 -0400

    Add vendored fakefs to the load path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "spec"
 require File.expand_path(File.dirname(__FILE__) + "/../lib/odb")
 
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + "/../vendor/fakefs/lib")
+
 Spec::Runner.configure do |config|
   config.before do
     Odb::ProcessIdMap.clear
