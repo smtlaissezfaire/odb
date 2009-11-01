f922fd34de7a9f20144126ba16a625ff4afbb4a5
Scott Taylor <scott@railsnewbie.com>
2009-11-01 16:38:41 -0500

    Refactoring ObjectIdFile

--- lib/odb/object_id_file.rb
+++ lib/odb/object_id_file.rb
@@ -1,7 +1,7 @@
 module Odb
   class ObjectIdFile
     include FileHelpers
-    
+
     NEW_LINE = "\n"
 
     def write(object, start, finish)
@@ -13,29 +13,37 @@ module Odb
     end
 
   private
-  
+
     def write_existing_object(object_id, start, finish)
       str = format_offsets(start, finish)
-      str << NEW_LINE unless line_count(path.objects_index) == 1
+      str << NEW_LINE unless line_count(objects_index) == 1
 
-      replace_line path.objects_index, object_id, str
+      replace_line(objects_index, object_id, str)
     end
-    
+
     def write_new_object(object, start, finish)
       str = ""
-      str << NEW_LINE unless empty_file?(path.objects_index)
+      str << NEW_LINE unless empty_file?(objects_index)
       str << format_offsets(start, finish)
-      
-      append_to_file path.objects_index, str
-      ProcessIdMap[object] = line_count(path.objects_index)
+
+      append_to_file(objects_index, str)
+      write_to_process_map(object)
     end
-  
+
+    def write_to_process_map(object)
+      ProcessIdMap[object] = line_count(objects_index)
+    end
+
     def format_offsets(start, finish)
       "#{start},#{finish}"
     end
-  
+
     def empty_file?(file)
       File.size(file) == 0
+    end
+
+    def objects_index
+      path.objects_index
     end
 
     def path
