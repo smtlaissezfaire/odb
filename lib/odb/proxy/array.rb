59f3ac7cd62a5fe32f9b2458169820a4c0866c86
Scott Taylor <scott@railsnewbie.com>
2009-11-02 20:30:29 -0500

    Refactor Proxy::Array to use a common method "lookup element"

--- lib/odb/proxy/array.rb
+++ lib/odb/proxy/array.rb
@@ -1,26 +1,30 @@
 module Odb
   module Proxy
     class Array < ::Array
-      def [](index)
-        Odb::Object.read(super)
+      def each
+        super do |oid|
+          yield lookup_element(oid)
+        end
       end
 
-      def each(&block)
-        to_a.each(&block)
+      def [](index)
+        lookup_element super
       end
 
       def first
-        self[0]
+        lookup_element super
       end
 
       def last
-        self[self.size - 1]
+        lookup_element super
       end
 
       alias_method :object_ids, :to_a
 
       def to_a
-        object_ids.map { |e| self[e] }
+        object_ids.map do |oid|
+          lookup_element oid
+        end
       end
 
       def inspect
@@ -29,6 +33,12 @@ module Odb
           str << object_ids.map { |id| "object_id:#{id}" }.inspect.gsub('"', '')
           str << ">"
         end
+      end
+
+    private
+
+      def lookup_element(object_id)
+        Odb::Object.read(object_id)
       end
     end
   end
